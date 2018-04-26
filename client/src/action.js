b0d404ea99d4ca35e56754cc62c3c0b9d0383d79
rattanapong-saelim <38746944+rattanapong-saelim@users.noreply.github.com>
2018-04-26 21:45:08 +0700

    Add files via upload

--- client/src/action.js
+++ client/src/action.js
@@ -17,3 +17,12 @@ export function deleteBear(id) {
     }
 
 }
+
+export function createBear(bear) {
+    return (dispatch) => {
+        axios.post(`http://localhost:8000/api/bears`, bear)
+        .then(() => {
+            dispatch(fetchBear())
+        })
+    }
+}
